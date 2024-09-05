487f1ba902b4136ac23a148f85db2cb8cccf5429
Guilherme <ludewon@gmail.com>
2024-09-05 00:16:37 -0300

    commit

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
            echo 'Olá mundo!';
+           echo "concluido!";
         ?>
     </body>
 </html>
